44918102e291695b7fc352b9bf2b6a25361aa46f
Bruno Minelli <minellibruno@hotmail.com>
2022-02-23 18:45:28 -0300

    Requisito 03 finalizado.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
-  onInputChange = ({ target }) => console.log(target);
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDestription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    console.log(`${target.name} ${target.value}`);
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
 
   onSaveButtonClick = (event) => {
     event.preventDefault();
@@ -10,22 +32,42 @@ class App extends React.Component {
   };
 
   render() {
+    const {
+      cardName,
+      cardDestription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
     return (
       <>
         <h1>Tryunfo</h1>
         <Form
-          cardName=""
-          cardDescription=""
-          cardAttr1=""
-          cardAttr2=""
-          cardAttr3=""
-          cardImage=""
-          cardRare=""
-          cardTrunfo={ false }
+          cardName={ cardName }
+          cardDescription={ cardDestription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
           hasTrunfo={ false }
           isSaveButtonDisabled={ false }
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDestription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </>
     );
